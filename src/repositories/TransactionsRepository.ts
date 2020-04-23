cb9bcf4bcba4925827802cd2d9d7d13b78138691
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-04-23 17:46:11 -0300

    Challenge solved

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (acumulator, object) =>
+        acumulator + (object.type === 'income' ? object.value : 0),
+      0,
+    );
+    const outcome = this.transactions.reduce(
+      (acumulator, object) =>
+        acumulator + (object.type === 'outcome' ? object.value : 0),
+      0,
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
